08105dfb1656cfee8bf338bf99efc00dce10e148
9a9k9m <9a9k9m@gmail.com>
2021-12-05 17:51:30 +0530

    terrafrom demo file

---
+++ demo_6/vars.tf
@@ -0,0 +1,11 @@
+variabele "AWS_REGION" {
+    default = "ap-southeast-1"
+}
+
+variable "PATH_TO_PRIVATE_KEY" {
+    default = "firstkey"
+}
+
+variable "PATH_TO_PUBLIC_KEY" {
+    default = "firstkey.pub"
+}

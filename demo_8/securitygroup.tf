e09daab9849771906ce271bd1546f35a5420a649
9a9k9m <9a9k9m@gmail.com>
2021-12-09 14:11:33 +0530

    midify

--- demo_8/securitygroup.tf
+++ demo_8/securitygroup.tf
@@ -4,8 +4,8 @@ resource "aws_security_group" "allow-ssh" {
 
     egress {
         from_port = 0
-        to_port  = 0
-        protocol = "-1"
+        to_port  = 65535
+        protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
 

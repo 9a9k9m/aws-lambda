40ee0bec1dcaca342536ce2c328221e4e12ec26b
9a9k9m <9a9k9m@gmail.com>
2021-12-10 17:24:54 +0530

    modify

--- demo_8/vpc.tf
+++ demo_8/vpc.tf
@@ -2,6 +2,9 @@
 
 resource "aws_vpc" "main" {
     cidr = "10.0.0.0/16"
+    instance_tenancy  = "default"
+    enable_dns_support = "true" 
+    enable_classiclink = "false"
 
     tags = {
         Name = "main"

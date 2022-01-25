2d97a9aa8b11f00c50e3b35d1f32283337ca9b31
9a9k9m <9a9k9m@gmail.com>
2022-01-25 21:30:59 +0530

    modify

--- vars.tf
+++ vars.tf
@@ -3,7 +3,6 @@ variable my_ip {}
 variable env-prefix {}
 variable image_name {}
 variable instance_type {}
-variable subnet_id {}
 variable avail_zone {}
 variable path_to_public_key {}
 variable subnet_cidr_block {}

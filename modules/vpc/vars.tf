d6667cacd4f9e890ce208c7f1d5f9cf776fdfff6
9a9k9m <9a9k9m@gmail.com>
2022-01-26 01:23:57 +0530

    terraform

---
+++ modules/vpc/vars.tf
@@ -0,0 +1,9 @@
+variable vpc_id {}
+variable vpc_cidr_block {}
+variable prefix{}
+vaiable subnet_cidr_block {}
+variable availability_zone {}
+variable availability_zone {}
+public_subnet_id{}
+private_subnet_id{}
+
